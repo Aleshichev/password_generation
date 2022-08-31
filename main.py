cf3be293f9058a8c1a02d8db8cebb81f29243661
Igor <Aleshichev.git@email.ua>
2022-08-31 16:21:42 +0300

    Законченный проект с поправками

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def generate_password():
-    entry_password.delete(0,END)
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-    password_letters = [choice(letters) for _ in range(randint(8, 10)) ]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for let in range(randint(2, 4)) ]
+
+def generate_password():
+    """This function generate new password"""
+
+    entry_password.delete(0,END)
+    password_letters = [choice(LETTERS) for _ in range(randint(8, 10)) ]
+    password_symbols = [choice(SYMBOLS) for _ in range(randint(2, 4))]
+    password_numbers = [choice(NUMBERS) for let in range(randint(2, 4)) ]
     # for let in range(nr_letters):
     #     password_lst += random.choice(letters)
     # for symb in range(nr_symbols):
@@ -23,14 +27,12 @@ def generate_password():
     password_lst = password_letters + password_numbers + password_symbols
     shuffle(password_lst)
     password = "".join(password_lst)
-    # password = ''
-    # for char in password_lst:
-    #     password += char
     entry_password.insert(0, password)
-    pyperclip.copy(password)      # автоматически сохраняет в буфер обмена пароль
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+    pyperclip.copy(password)      # save password in clipboard
+
 
 def save():
+    """This fuction save new password"""
 
     website = entry_web.get()
     email = entry_email.get()
@@ -50,26 +52,22 @@ def save():
         if is_ok:
             try:
                 with open("data.json", "r") as data_file:
-                    # data_file.write(f"{website} | {email} | {password}\n")
-                    # Reading
                     data = json.load(data_file)
             except FileNotFoundError:
                 with open("data.json", "w") as data_file:
                     json.dump(new_data, data_file, indent=4)
             else:
-                    # Updating
                 data.update(new_data)
-
                 with open("data.json", "w") as data_file:
-                    # Saving
                     json.dump(data, data_file, indent=4)
-
             finally:
                 entry_web.delete(0, END)
                 entry_password.delete(0, END)
 
-# ---------------------------- Find Password ------------------------------- #
+
 def find_password():
+    """This function find password"""
+
     website = entry_web.get()
     try:
         with open("data.json") as data_file:
@@ -80,12 +78,12 @@ def find_password():
         if website in data:
             email = data[website]["email"]
             password = data[website]["password"]
+            pyperclip.copy(password)
             messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
         else:
             messagebox.showinfo(title="Ошибка!!!", message=f"Нет данных {website}")
 
-# ---------------------------- UI SETUP ------------------------------- #
-
+# ------------------------------------------ UI ------------------------------------------------------------ #
 
 window = Tk()
 window.resizable(False, False)
@@ -98,6 +96,8 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
+#-------------------labels------------------
+
 label_web = Label(text="Website: ")
 label_web.grid(column=0, row=1)
 
@@ -107,7 +107,8 @@ label_email.grid(column=0, row=2)
 label_password = Label(text="Password:")
 label_password.grid(column=0, row=3)
 
-#buttons
+#-------------------buttons-------------------
+
 search_button = Button(text="Search", width=14, command=find_password)
 search_button.grid(row=1, column=2)
 
@@ -116,6 +117,8 @@ button_password.grid(column=2, row=3)
 
 button_add = Button(text="Add", width=44, command=save)
 button_add.grid(column=1, row=4, columnspan=2)
+
+#-------------------entry-------------------
 
 entry_web = Entry(width=33)
 entry_web.grid(column=1, row=1)
@@ -127,8 +130,6 @@ entry_email.insert(0, "igor@gmail.com")
 
 entry_password = Entry(width=33)
 entry_password.grid(column=1, row=3)
-
-
 
 
 window.mainloop()
